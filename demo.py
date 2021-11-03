865ea4f72ff9b72b4943551f25c954948aeee46c
FaizanM2000 <faizanmohammed635@gmail.com>
2021-11-03 15:08:00 -0500

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.set_page_config( layout='wide')
-st.markdown("<h1 style='text-align: center; color: white;'>OAD Industry Classification Model</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: black;'>OAD Industry Classification Model</h1>", unsafe_allow_html=True)
